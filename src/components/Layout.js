a1fffe408070d717b4381e513fc13bb7be55d36e
Fernando Sandmann <sis.argentina@gmail.com>
2023-04-26 10:04:49 -0300

    changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, Typography } from "@mui/material";
-import Divider from '@mui/material/Divider';
 import { Link } from "react-router-dom";
-import BackupTableIcon from "@mui/icons-material/BackupTable";
+
 import ViewQuiltIcon from "@mui/icons-material/ViewQuilt";
+import BackupTableIcon from "@mui/icons-material/BackupTable";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 import NotificationsIcon from "@mui/icons-material/Notifications";
@@ -13,13 +13,12 @@ import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import BuildCircleIcon from "@mui/icons-material/BuildCircle";
 import Logo from "../assets/images/logo.png";
 
-const Item = ({ title, to, icon, selected, setSelected }) => {
+const Item = memo(({ title, to, icon, selected, setSelected }) => {
   return (
     <MenuItem
+      key={title}
       active={selected === title}
-      style={{
-        color: "white",
-      }}
+      className="menu-item"
       onClick={() => setSelected(title)}
       icon={icon}
     >
@@ -27,7 +26,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
       <Link to={to} />
     </MenuItem>
   );
-};
+});
 
 const SidebarLayout = ({ children }) => {
   const [selected, setSelected] = useState("Dashboard");
@@ -37,8 +36,8 @@ const SidebarLayout = ({ children }) => {
       <Box
         sx={{
           "& .ps-menu-button ": {
-            borderRadius:"10px",
-            margin:"5px"
+            borderRadius: "10px",
+            margin: "5px",
           },
           "& .ps-menu-button:hover ": {
             background: `rgba(255, 255, 255, 0.2)`,
@@ -50,74 +49,41 @@ const SidebarLayout = ({ children }) => {
       >
         <Sidebar className="sidebar">
           <Menu>
-          <Box mb="25px">
-              <Box display="flex" justifyContent="center" alignItems="center" style={{ margin: "20px"}}>
+            <Box mb="25px">
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                style={{ margin: "20px" }}
+              >
                 <img
                   alt="brand"
                   width="100px"
                   height="100px"
                   src={Logo}
-                  style={{ cursor: "pointer", height: "65px"}}
+                  style={{ cursor: "pointer", height: "65px" }}
                 />
               </Box>
             </Box>
-            <hr className="faded-out-hr"/>
-            <Item
-              title="Dashboard"
-              to="/"
-              icon={<ViewQuiltIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Reportes"
-              to="/"
-              icon={<BackupTableIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Facturación"
-              to="/"
-              icon={<ReceiptLongIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Cobranzans"
-              to="/"
-              icon={<CurrencyExchangeIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Notificaciones"
-              to="/"
-              icon={<NotificationsIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Usuarios"
-              to="/"
-              icon={<PeopleAltIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Altas"
-              to="/"
-              icon={<GroupAddIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Configuración"
-              to="/"
-              icon={<BuildCircleIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            <hr className="faded-out-hr" />
+            {[
+              { title: "Dashboard", icon: <ViewQuiltIcon /> },
+              { title: "Reportes", icon: <BackupTableIcon /> },
+              { title: "Facturación", icon: <ReceiptLongIcon /> },
+              { title: "Cobranzans", icon: <CurrencyExchangeIcon /> },
+              { title: "Notificaciones", icon: <NotificationsIcon /> },
+              { title: "Usuarios", icon: <PeopleAltIcon /> },
+              { title: "Altas", icon: <GroupAddIcon /> },
+              { title: "Configuración", icon: <BuildCircleIcon /> },
+            ].map(({ title, icon }) => (
+              <Item
+                title={title}
+                to="/"
+                icon={icon}
+                selected={selected}
+                setSelected={setSelected}
+              />
+            ))}
           </Menu>
         </Sidebar>
       </Box>
